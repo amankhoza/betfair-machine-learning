90586d06805aa9eeaa1ff7b9331cca8e4ccbf1d8
Aman Khosa <amankhoza@hotmail.co.uk>
2016-12-24 17:49:45 +0000

    Add basic logging, closes #5 for now

--- betfair.py
+++ betfair.py
@@ -16,12 +16,18 @@ import threading
 # and get session id
 # to play around with visual API tool visit https://developer.betfair.com/exchange-api/betting-api-demo/
 
+# make call to api and return response as a python object
 def callApi(endpoint,jsonrpc_req):
     try:
         req = urllib.request.Request(endpoint, jsonrpc_req.encode('utf-8'), headers)
         response = urllib.request.urlopen(req)
         jsonResponse = response.read()
-        return jsonResponse.decode('utf-8')
+        jsonString = jsonResponse.decode('utf-8')
+        pythonObject = json.loads(jsonString)
+        if pythonObject.get('error'): # log api call errors to text file
+            log_out = open('./log/api_call_error.log', 'a')
+            log_out.write(time.ctime()+' - '+str(pythonObject.get('error'))+'\n'+str(pythonObject)+'\n')
+        return pythonObject
     except urllib.error.URLError as e:
         print (e.reason)
         print ('Oops no service available at ' + str(endpoint))
@@ -32,11 +38,9 @@ def callApi(endpoint,jsonrpc_req):
 
 def keepSessionAlive():
     keep_alive_req = ''
-    keep_alive_response = callApi(keep_alive_endpoint,keep_alive_req)
-    keep_alive_loads = json.loads(keep_alive_response)
-    #print(time.ctime())
-    #print ('called keep alive: ',keep_alive_loads['status'])
-    #maybe log these into a log file
+    keep_alive = callApi(keep_alive_endpoint,keep_alive_req)
+    keep_alive_out = open('./log/last_keep_alive.log', 'w')
+    keep_alive_out.write('Last keep alive called at: '+time.ctime()+'\n'+'Status: '+keep_alive['status']+'\n')
     threading.Timer(600, keepSessionAlive).start()
 
 def getMarketCatalogue(eventTypeID,competitionIds,marketCountries,marketTypes):
@@ -45,10 +49,9 @@ def getMarketCatalogue(eventTypeID,competitionIds,marketCountries,marketTypes):
         end_time = (datetime.datetime.now() + datetime.timedelta(hours=2)).strftime('%Y-%m-%dT%H:%M:%SZ')
         # these start & end time filters ensure data is collected from 2 hours before ko, up to 2 hours after ko
         market_catalogue_req = ('{"jsonrpc": "2.0", "method": "SportsAPING/v1.0/listMarketCatalogue", "params": {"filter":{"eventTypeIds":["' + eventTypeID + '"],"competitionIds":["' + competitionIds + '"],"marketCountries":["' + marketCountries + '"],"marketTypeCodes":["' + marketTypes + '"], "marketStartTime":{"from":"' + start_time + '","to":"' + end_time + '"}},"sort":"FIRST_TO_START","maxResults":"100","marketProjection":["EVENT","RUNNER_METADATA"]}, "id": 1}')
-        market_catalogue_response = callApi(betting_endpoint,market_catalogue_req)
-        market_catalouge_loads = json.loads(market_catalogue_response)
+        market_catalogue = callApi(betting_endpoint,market_catalogue_req)
         try:
-            market_catalouge_results = market_catalouge_loads['result']
+            market_catalouge_results = market_catalogue['result']
             return market_catalouge_results
         except:
             print ('Exception from API-NG' + str(market_catalouge_results['error']))
@@ -58,21 +61,7 @@ def getEplMarketCatalogue():
     return getMarketCatalogue('1','31','','MATCH_ODDS')
 
 def getCustomMarketCatalogue():
-    return getMarketCatalogue('1','8068520','','MATCH_ODDS')
-
-def getRunnerMappings(market):
-    mappings = {}
-    for runner in market['runners']:
-        mappings[runner['selectionId']] = str(runner['runnerName'])
-    return mappings
-
-# def getRunnerData(runners):
-#     runnerData = {}
-#     for runner in runners:
-#         runnerData[runner['selectionId']] = {}
-#         runnerData[runner['selectionId']]['back'] = runner['ex']['availableToBack']
-#         runnerData[runner['selectionId']]['lay'] = runner['ex']['availableToLay']
-#     return runnerData
+    return getMarketCatalogue('1','2005','','MATCH_ODDS')
 
 def extractOddsAsString(runnerString):
     n = len(runnerString)
@@ -103,15 +92,20 @@ def getRunnerDataString(runners):
     return runnerDataString
 
 def getMarketBookBestOffers(marketId):
-    market_book_req = '{"jsonrpc": "2.0", "method": "SportsAPING/v1.0/listMarketBook", "params": {"marketIds":["' + marketId + '"],"priceProjection":{"priceData":["EX_BEST_OFFERS"]}}, "id": 1}'
-    market_book_response = callApi(betting_endpoint,market_book_req)
-    market_book_loads = json.loads(market_book_response)
+    #market_book_req = '{"jsonrpc": "2.0", "method": "SportsAPING/v1.0/listMarketBook", "params": {"marketIds":["' + marketId + '"],"priceProjection":{"priceData":["EX_BEST_OFFERS"]}}, "id": 1}' #without virtualise
+    market_book_req = '{"jsonrpc": "2.0", "method": "SportsAPING/v1.0/listMarketBook", "params": {"marketIds":["' + marketId + '"],"priceProjection":{"priceData":["EX_BEST_OFFERS"],"virtualise":"true"}}, "id": 1}' #with virtualise
+    market_book = callApi(betting_endpoint,market_book_req)
     try:
-        market_book_result = market_book_loads['result'][0]
+        market_book_result = market_book['result'][0]
         return market_book_result
     except:
         print  ('Exception from API-NG' + str(market_book_result['error']))
         exit()
+
+def createDirectories(directories):
+    for directory in directories:
+        if not os.path.exists('./'+directory):
+            os.makedirs('./'+directory)
 
 args = len(sys.argv)
 
@@ -128,15 +122,14 @@ betting_endpoint = "https://api.betfair.com/exchange/betting/json-rpc/v1"
 keep_alive_endpoint = "https://identitysso.betfair.com/api/keepAlive"
 headers = {'x-application': appKey, 'x-authentication': sessionToken, 'content-type': 'application/json', 'accept': 'application/json'}
 
-if not os.path.exists('./data'):
-    os.makedirs('./data')
+createDirectories(['data','log'])
 
 keepSessionAlive()
 
 while True:
 
-    #marketCatalogueResult = getEplMarketCatalogue()
-    marketCatalogueResult = getCustomMarketCatalogue()
+    marketCatalogueResult = getEplMarketCatalogue()
+    #marketCatalogueResult = getCustomMarketCatalogue()
 
     #print (marketCatalogueResult)
 
@@ -147,10 +140,8 @@ while True:
         eventName = market['event']['name'].strip()
         eventDate = market['event']['openDate'].split('T')[0]
         eventTime = market['event']['openDate'].split('T')[1][:5]
-        runnerMappings = getRunnerMappings(market) #ignoring since we don't use runner mapping id's etc
         filename = eventDate+'_'+eventTime+'_'+eventName+'.csv'
         filepath = './data/'+filename
-        #print (eventName,'\n',eventDate,'\n',runnerMappings,'\n')
 
         if (os.path.isfile(filepath)):
             fo = open(filepath, 'a')
@@ -162,7 +153,7 @@ while True:
             # 1 = best odds, 2 = second best odds, 3 = third best odds
             # For back odds higher price is better
             # For lay odds lower price is better
-            
+
         market_book_result = getMarketBookBestOffers(marketid)
 
         current_time = datetime.datetime.now()
