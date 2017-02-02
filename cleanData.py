d98be3794300c56481ac6586cd94804657548392
Aman Khosa <amankhoza@hotmail.co.uk>
2017-02-02 12:29:11 +0000

    Remove anomalous odds from end of match, fixes #8

--- cleanData.py
+++ cleanData.py
@@ -44,6 +44,34 @@ def cleanFile(directory,fileName):
 
     return errors
 
+def removeOddsFromEndOfMatch(directory,fileName):
+    df = pd.read_csv(directory+fileName)
+
+    statusColumn = df['Status']
+
+    n = len(statusColumn)
+    i = n-1
+
+    while (statusColumn[i] == 'SUSPENDED'):
+        i -= 1
+
+    finalSuspendIndex = i+1 # add one for headers
+
+    errors = n - finalSuspendIndex
+
+    f = open(directory+fileName,'r+')
+
+    for i in range(0,n-1):
+        currentLine = f.readline()
+
+        if (i == finalSuspendIndex):
+            f.truncate()
+            break
+
+    f.close()
+
+    return errors
+
 directory = sys.argv[1] if sys.argv[1].endswith('/') else sys.argv[1]+'/'
 cleanDirectory = directory+cleanedFilesFolderName+'/'
 
@@ -51,8 +79,13 @@ if os.path.exists(cleanDirectory):
     print('ERROR CLEANING HAS ALREADY BEEN RAN ON THIS DIRECTORY')
 else:
     os.makedirs(cleanDirectory)
+    print('\nREMOVING NOISE:\n')
     for fileName in os.listdir(directory):
         if not fileName.endswith('.csv'):
             continue # skip directories and other files
         errorsFixed = cleanFile(directory,fileName)
+        print(fileName+': '+str(errorsFixed)+' errors fixed')
+    print('\nREMOVING ODDS FROM END OF MATCH:\n')
+    for fileName in os.listdir(cleanDirectory):
+        errorsFixed = removeOddsFromEndOfMatch(cleanDirectory,fileName)
         print(fileName+': '+str(errorsFixed)+' errors fixed')
