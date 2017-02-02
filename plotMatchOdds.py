08a67647aea11745df13739fd1af3d7f0bbe6488
Aman Khosa <amankhoza@hotmail.co.uk>
2017-02-02 14:39:31 +0000

    Add new figure for lay odds, closes #9

--- plotMatchOdds.py
+++ plotMatchOdds.py
@@ -2,6 +2,12 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import sys
 
+def setPlotLabels(matchTitle,matchDate):
+    plt.xlabel('time')
+    plt.ylabel('odds')
+    plt.legend(loc='upper left')
+    plt.title(matchTitle+' on '+matchDate)
+
 plt.style.use('ggplot')
 
 filePath = sys.argv[1]
@@ -13,17 +19,24 @@ awayTeam = matchTitle.split(' v ')[1]
 
 df = pd.read_csv(filePath)
 
+time = df['Time']
 hbOdds = df['HB1 odds']
+hlOdds = df['HL1 odds']
 abOdds = df['AB1 odds']
+alOdds = df['AL1 odds']
 dbOdds = df['DB1 odds']
+dlOdds = df['DL1 odds']
 
-plt.plot(hbOdds,label=homeTeam)
-plt.plot(abOdds,label=awayTeam)
-plt.plot(dbOdds,label='Draw')
+plt.figure(1)
+plt.plot(hbOdds,label=homeTeam+' back')
+plt.plot(abOdds,label=awayTeam+' back')
+plt.plot(dbOdds,label='Draw back')
+setPlotLabels(matchTitle,matchDate)
 
-plt.xlabel('time')
-plt.ylabel('odds')
-plt.legend(loc='upper left')
-plt.title(matchTitle+' on '+matchDate)
+plt.figure(2)
+plt.plot(hlOdds,label=homeTeam+' lay')
+plt.plot(alOdds,label=awayTeam+' lay')
+plt.plot(dlOdds,label='Draw lay')
+setPlotLabels(matchTitle,matchDate)
 
 plt.show()
