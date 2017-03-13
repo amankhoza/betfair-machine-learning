ed5588f69209369b95132f40bea1c2884de626a1
Aman Khosa <amankhoza@hotmail.co.uk>
2017-03-13 20:15:48 +0000

    Rankings scraper

---
+++ rankingsScraper.py
@@ -0,0 +1,30 @@
+import urllib
+import urllib.error
+from bs4 import BeautifulSoup
+
+
+def convertNameToBetfairName(name):
+    extensions = ['City', 'United', 'Hotspur', 'Albion', 'rystal', 'wich']
+    if 'Man' in name:
+        name = name.replace('chester', '')
+        name = name.replace('United', 'Utd')
+    else:
+        for ext in extensions:
+            name = name.replace(ext, '')
+    return name
+
+
+try:
+    web_page = urllib.request.urlopen("http://www.bbc.co.uk/sport/football/premier-league/table")
+    soup = BeautifulSoup(web_page, 'lxml')
+    teams = [td.find('a').getText() for td in soup.findAll('td', class_='team-name')[0:20]]
+    out = open('rankings.csv', 'w')
+    headers = 'Ranking,Team'
+    out.write(headers+'\n')
+    for i in range(0, len(teams)):
+        out.write(str(i+1)+','+convertNameToBetfairName(teams[i])+'\n')
+    out.close()
+except urllib.error.HTTPError:
+    print("HTTPERROR!")
+except urllib.error.URLError:
+    print("URLERROR!")
